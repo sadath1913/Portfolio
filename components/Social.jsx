e38c4235a431c77931426c5631ac7d7357a69600
sadath1913 <sadathkhan717@gmail.com>
2026-09-12 12:01:55 +0530

    Added Email

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,25 +1,57 @@
 import Link from "next/link";
-import { FaGithub, FaLinkedinIn, FaInstagram, FaEnvelope  } from "react-icons/fa"
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaInstagram,
+  FaEnvelope,
+} from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub/>, path: "https://github.com/sadath1913"},
-    {icon: <FaLinkedinIn/>, path: "https://linkedin.com/in/sadath-khan-535b56293"},
-    {icon: <FaInstagram/>, path: "https://www.instagram.com/sadathkhan_1913?igsi=MW90NDM5MnlyZ25jeQ==", label: "Instagram"},
-    {icon: <FaEnvelope/>, path: "mailto:sadathkhan1913@gmail.com", label: "Email"}
-]
+  {
+    icon: <FaGithub />,
+    path: "https://github.com/sadath1913",
+    label: "GitHub",
+  },
+  {
+    icon: <FaLinkedinIn />,
+    path: "https://linkedin.com/in/sadath-khan-535b56293",
+    label: "LinkedIn",
+  },
+  {
+    icon: <FaInstagram />,
+    path: "https://www.instagram.com/sadathkhan_1913?igsi=MW90NDM5MnlyZ25jeQ==",
+    label: "Instagram",
+  },
+  {
+    icon: <FaEnvelope />,
+    path: "mailto:sadathkhan717@gmail.com",
+    label: "Email",
+  },
+];
 
-const Social = ({containerStyles, iconStyles}) => {
-  return ( 
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
     <div className={containerStyles}>
-        {socials.map((item,index)=>{
-            return (
-                <Link key={index} href={item.path} aria-label={item.label} className={iconStyles}>
-                    {item.icon}
-                </Link>
-            );
-        })}
+      {socials.map((item, index) => {
+        const isEmail = item.path.startsWith("mailto:");
+
+        return (
+          <Link
+            key={index}
+            href={item.path}
+            aria-label={item.label}
+            className={iconStyles}
+            {...(!isEmail && {
+              target: "_blank",
+              rel: "noopener noreferrer",
+            })}
+          >
+            {item.icon}
+          </Link>
+        );
+      })}
     </div>
   );
-}
+};
 
-export default Social
+export default Social;
